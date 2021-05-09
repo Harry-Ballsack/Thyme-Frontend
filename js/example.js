2c98aaa080dd30924ebd90d319f891cc24c1ef40
TechnoElf <technoelf@undertheprinter.com>
2021-05-09 13:16:37 +0200

    Add user registration example

--- js/example.js
+++ js/example.js
@@ -1,24 +1,39 @@
+const reg_name_fld = document.getElementById("reg_name_fld");
+const reg_login_fld = document.getElementById("reg_login_fld");
+const reg_pass_fld = document.getElementById("reg_pass_fld");
+const register_btn = document.getElementById("register_btn");
+const register_lbl = document.getElementById("register_lbl");
+
 const login_fld = document.getElementById("login_fld");
 const pass_fld = document.getElementById("pass_fld");
 const login_btn = document.getElementById("login_btn");
+const login_lbl = document.getElementById("login_lbl");
 const name_lbl = document.getElementById("name_lbl");
 const stations_lbl = document.getElementById("stations_lbl");
+
 const station_lbl = document.getElementById("station_lbl");
 const state_lbl = document.getElementById("state_lbl");
 const data_div = document.getElementById("data_div");
 
+register_btn.addEventListener("click", register);
 login_btn.addEventListener("click", login);
 
+async function register() {
+    await create_user(reg_name_fld.value, reg_login_fld.value, reg_pass_fld.value);
+    register_lbl.textContent = "Ok";
+}
+
 async function login() {
-    get_user(login_fld.value, pass_fld.value).then(d => name_lbl.textContent = d.name);
+    get_user(login_fld.value, pass_fld.value).then(d => {
+        login_lbl.textContent = "Ok";
+        name_lbl.textContent = d.name;
+    });
+    get_stations(login_fld.value, pass_fld.value).then(d => {
+        stations_lbl.textContent = d.join(", ");
+    });
+}
 
-    const stations = await get_stations(login_fld.value, pass_fld.value);
-    var list = "";
-    for (var i = 0; i < stations.length; i++) {
-        list += stations[i] + " ";
-    }
-    stations_lbl.textContent = list;
-
+/*
     get_station(stations[0], login_fld.value, pass_fld.value).then(d => station_lbl.textContent = d.name);
     get_state(stations[0], login_fld.value, pass_fld.value).then(d => state_lbl.textContent = d);
 
@@ -33,4 +48,4 @@ async function login() {
         table += "</tr>";
     }
     data_div.innerHTML = table;
-}
+*/
