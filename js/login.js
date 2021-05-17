904d598f1bf66136eb911bfe187d7fe0fd486cd7
JRoss <63255895+Harry-Ballsack@users.noreply.github.com>
2021-05-17 22:20:46 +0200

    Implementing login

--- js/login.js
+++ js/login.js
@@ -5,9 +5,14 @@ const passwdFld = document.getElementById("passwdFld");
 loginBtn.addEventListener("click", login);
 
 async function login() {
-    get_user(userFld.value, passwdFld.value).then(d => {
+    get_user(userFld.value, passwdFld.value).then(
+		function(response) {
+			if(response.status !== 200) {
+				console.log(response);
+			}
+			return;
+		}
         console.log(d.name);
-		
     });
     get_stations(userFld.value, passwdFld.value).then(d => {
         console.log(d.join(", "));
