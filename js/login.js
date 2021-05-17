57089db9d3fedb889dd4d1f84e69b635d4d0998c
JRoss <63255895+Harry-Ballsack@users.noreply.github.com>
2021-05-17 22:46:58 +0200

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -5,23 +5,13 @@ const passwdFld = document.getElementById("passwdFld");
 loginBtn.addEventListener("click", login);
 
 async function login() {
-    get_user(userFld.value, passwdFld.value).then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    }
-    )
-    .catch(function(err) {
-		console.log('Fetch Error :-S', err);
-    });
+    get_user(userFld.value, passwdFld.value).then(d => {
+        console.log(d.name);
+		
+    })
+	.catch( error => {
+		console.log(error);
+	});
 	
     get_stations(userFld.value, passwdFld.value).then(d => {
         console.log(d.join(", "));
