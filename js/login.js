8f3bb0274972cc45396a195c5eed101fb9699504
JRoss <63255895+Harry-Ballsack@users.noreply.github.com>
2021-05-17 22:12:13 +0200

    Syntax is hard

--- js/login.js
+++ js/login.js
@@ -5,11 +5,11 @@ const passwdFld = document.getElementById("passwdFld");
 loginBtn.addEventListener("click", login);
 
 async function login() {
-    get_user(login_fld.value, pass_fld.value).then(d => {
+    get_user(userFld.value, passwdFld.value).then(d => {
         console.log(d.name);
 		
     });
-    get_stations(login_fld.value, pass_fld.value).then(d => {
+    get_stations(userFld.value, passwdFld.value).then(d => {
         console.log(d.join(", "));
     });
 }
