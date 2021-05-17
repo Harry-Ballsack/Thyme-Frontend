e1707a5109c023662eb587bbad7b15075ae7808a
JRoss <63255895+Harry-Ballsack@users.noreply.github.com>
2021-05-17 22:34:14 +0200

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -5,19 +5,10 @@ const passwdFld = document.getElementById("passwdFld");
 loginBtn.addEventListener("click", login);
 
 async function login() {
-    get_user(userFld.value, passwdFld.value).then(
-		function(response) {
-			if(response.status !== 200) {
-				console.log('Problem status: ' + response.status);
-			}
-			return;
-		}
+    get_user(userFld.value, passwdFld.value).then(d => {
         console.log(d.name);
-    )
-	.catch(function(e) {
-		console.log('Error: ' + e);
-	});
-	
+		
+    });
     get_stations(userFld.value, passwdFld.value).then(d => {
         console.log(d.join(", "));
     });
